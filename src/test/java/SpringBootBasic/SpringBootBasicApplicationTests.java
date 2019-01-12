5c14f93fe4355136cede829f0dc4d4ab299f00b7
InSeong-So <goflvhxj2547@gmail.com>
2019-01-12 19:40:10 +0900

    spring-boot study용 저장소 생성

---
+++ src/test/java/SpringBootBasic/SpringBootBasicApplicationTests.java
@@ -0,0 +1,17 @@
+package SpringBootBasic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
